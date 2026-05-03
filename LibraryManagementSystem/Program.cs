858514d825997843a97ac2ee9f8fca3e1104aaf3
Ash Syed <20032206@student.com>
2026-05-04 00:54:13 +1000

    Fix SQLite database path for Azure deployment

--- LibraryManagementSystem/Program.cs
+++ LibraryManagementSystem/Program.cs
@@ -1,41 +1,53 @@
-using LibraryManagementSystem.Data;
+﻿using LibraryManagementSystem.Data;
 using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<LibraryDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddAuthentication("LibraryCookie")
-    .AddCookie("LibraryCookie", options =>
-    {
-        options.LoginPath = "/Account/Login";
-    });
 
-builder.Services.AddAuthorization();
+// Configure SQLite Database with LibraryDbContext
+builder.Services.AddDbContext<LibraryDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+// Auto-create database on startup (Azure Fix)
+using (var scope = app.Services.CreateScope())
+{
+    try
+    {
+        var db = scope.ServiceProvider.GetRequiredService<LibraryDbContext>();
+        var dbPath = db.Database.GetDbConnection().DataSource;
+        var dbDirectory = Path.GetDirectoryName(dbPath);
+        
+        if (!string.IsNullOrEmpty(dbDirectory) && !Directory.Exists(dbDirectory))
+        {
+            Directory.CreateDirectory(dbDirectory);
+        }
+        
+        db.Database.EnsureCreated();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Database initialization error: {ex.Message}");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseRouting();
-
-app.UseAuthentication();
 app.UseAuthorization();
-
-app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
